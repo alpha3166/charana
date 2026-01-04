9973bcc1ca8d1731d8262e079741bf0f7f7aa327
alpha3166 <foxtrot3166-git@yahoo.co.jp>
2026-01-04 02:42:37 +0000

    faet(web): Update to Spring Boot 4.0

--- charana-web/src/test/java/alpha3166/charana/web/ApiControllerTest.java
+++ charana-web/src/test/java/alpha3166/charana/web/ApiControllerTest.java
@@ -9,13 +9,13 @@ import java.nio.charset.StandardCharsets;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
